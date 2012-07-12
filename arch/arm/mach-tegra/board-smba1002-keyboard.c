42acbabcf70493d57383fae244fe34601ab2ac6b
slyking241 <slyking241@gmail.com>
2012-07-12 15:22:53 -0300

    Changes to make buttons work{Honeycomb}

--- arch/arm/mach-tegra/board-smba1002-keyboard.c
+++ arch/arm/mach-tegra/board-smba1002-keyboard.c
@@ -2,7 +2,7 @@
 /*
  * arch/arm/mach-tegra/board-smba1002-keyboard.c
  *
- * Copyright (C) 2011 Eduardo Jos� Tagle <ejtagle@tutopia.com>
+ * Copyright (C) 2011 Eduardo José Tagle <ejtagle@tutopia.com>
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -27,11 +27,12 @@
 #include "board-smba1002.h"
 #include "gpio-names.h"
 
+
 static struct gpio_keys_button smba1002_keys[] = {
 	[0] = {
 		.gpio = SMBA1002_KEY_VOLUMEUP,
 		.active_low = true,
-		.debounce_interval = 20,
+		.debounce_interval = 10,
 		.wakeup = false,		
 		.code = KEY_VOLUMEUP,
 		.type = EV_KEY,		
@@ -40,7 +41,7 @@ static struct gpio_keys_button smba1002_keys[] = {
 	[1] = {
 		.gpio = SMBA1002_KEY_VOLUMEDOWN,
 		.active_low = true,
-		.debounce_interval = 20,
+		.debounce_interval = 10,
 		.wakeup = false,		
 		.code = KEY_VOLUMEDOWN,
 		.type = EV_KEY,		
@@ -49,29 +50,36 @@ static struct gpio_keys_button smba1002_keys[] = {
 	[2] = {
 		.gpio = SMBA1002_KEY_POWER,
 		.active_low = true,
-		.debounce_interval = 50,
+		.debounce_interval = 100,
 		.wakeup = true,		
 		.code = KEY_POWER,
 		.type = EV_KEY,		
 		.desc = "power",
 	},
-	/*[3] = {
-		.gpio = SMBA1002_KEY_BACK,
-		.active_low = false,
-		.debounce_interval = 20,
-		.wakeup = true,		
+/*	[3] = {
+		.gpio = SMBA1007_KEY_BACK,
+		.active_low = true,
+		.debounce_interval = 10,
+		.wakeup = false,		
 		.code = KEY_BACK,
 		.type = EV_KEY,		
 		.desc = "back",
 	},*/
-	
 };
+#define PMC_WAKE_STATUS 0x14
 
+static int smba1002_wakeup_key(void)
+{
+	unsigned long status = 
+		readl(IO_ADDRESS(TEGRA_PMC_BASE) + PMC_WAKE_STATUS);
+	return status & TEGRA_WAKE_GPIO_PV2 ? KEY_POWER : KEY_RESERVED;
+}
 
 static struct gpio_keys_platform_data smba1002_keys_platform_data = {
 	.buttons 	= smba1002_keys,
 	.nbuttons 	= ARRAY_SIZE(smba1002_keys),
-	.rep		= false, /* auto repeat disabled */
+	.wakeup_key     = smba1002_wakeup_key,
+	.rep		= false, /* auto repeat enabled */
 };
 
 static struct platform_device smba1002_keys_device = {
@@ -82,6 +90,7 @@ static struct platform_device smba1002_keys_device = {
 	},
 };
 
+
 static struct platform_device *smba1002_pmu_devices[] __initdata = {
 	&smba1002_keys_device,
 };
@@ -89,6 +98,6 @@ static struct platform_device *smba1002_pmu_devices[] __initdata = {
 /* Register all keyboard devices */
 int __init smba1002_keyboard_register_devices(void)
 {
+  	//enable_irq_wake(gpio_to_irq(TEGRA_WAKE_GPIO_PV2));
 	return platform_add_devices(smba1002_pmu_devices, ARRAY_SIZE(smba1002_pmu_devices));
 }
-
